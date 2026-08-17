914a71e0c13eaf16edc74ee8a60b4ff55bc4fd6a
LocaVerify <85289645+domdom68@users.noreply.github.com>
2026-08-17 15:23:04 +0200

    Fix: /paiement et /paiement/succes accessibles sans compte (bloquaient l'achat invité au niveau du routage)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -51,11 +51,16 @@ export default function App() {
           {/* Private */}
           <Route path="/dashboard" element={<PrivateRoute><Layout><Dashboard /></Layout></PrivateRoute>} />
           <Route path="/analyser" element={<PrivateRoute><Layout><Analyse /></Layout></PrivateRoute>} />
-          <Route path="/paiement" element={<PrivateRoute><Layout><Paiement /></Layout></PrivateRoute>} />
+          {/* /paiement et /paiement/succes sont volontairement PUBLICS : le Pack
+              Vacances peut être acheté sans compte préalable (voir Paiement.js /
+              webhook.js), donc ces deux pages doivent rester accessibles à un
+              visiteur non connecté. Paiement.js et PaymentSuccess.js gèrent déjà
+              un profil/utilisateur null sans planter. */}
+          <Route path="/paiement" element={<Layout><Paiement /></Layout>} />
+          <Route path="/paiement/succes" element={<Layout><PaymentSuccess /></Layout>} />
           <Route path="/rapport/:id" element={<PrivateRoute><Layout><Rapport /></Layout></PrivateRoute>} />
           <Route path="/communaute" element={<PrivateRoute><Layout><Communaute /></Layout></PrivateRoute>} />
           <Route path="/verifpaiement" element={<PrivateRoute><Layout><VerifPaiement /></Layout></PrivateRoute>} />
-          <Route path="/paiement/succes" element={<PrivateRoute><Layout><PaymentSuccess /></Layout></PrivateRoute>} />
 
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
